96630fbaf451531c79436912aa9665680cf6989d
Soren <swh9094@gmail.com>
2026-09-06 18:36:29 +0800

    feat: day01 ts基础类型练习

---
+++ day01-base.ts
@@ -0,0 +1,14 @@
+// day01 TS基础类型练习
+// number 数字类型
+let age: number = 18;
+let score: number = 95.5;
+
+// string 字符串类型
+let username: string = "Soren";
+
+// boolean布尔类型
+let isStudy: boolean = true;
+
+console.log("age:", age);
+console.log("username:", username);
+console.log("是否在学习TS：", isStudy);
